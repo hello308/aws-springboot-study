49c33d68042bebf4b2e3a5c7dae9b37f57900b79
hello308 <afqrsdwe@gmail.com>
2020-03-02 20:47:00 +0900

    챕터4. 게시글 조회 기능만들기

--- src/main/java/com/noop/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/noop/study/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.noop.study.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
-
+    // @Query 는 SpringDataJpa에서 제공하지 않는 메소드만 사용한다.
+    // 하단의 쿼리는 Jpa만으로 사용가능하나, 예시를 보여준거임
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/noop/study/springboot/web/IndexController.java
+++ src/main/java/com/noop/study/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.noop.study.springboot.web;
 
+import com.noop.study.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {      // Model 서버 템플릿 엔진에서 사용 할 수 있는 객체를 저장한다.
+                                            // postsService.findAllDesc() 에서 가져온 결과를 posts로 index.mustache에 전달한다.
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
