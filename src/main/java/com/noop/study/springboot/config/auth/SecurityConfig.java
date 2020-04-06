6a79ba7124c42374a1a2b1673f5e034ee8913520
hello308 <afqrsdwe@gmail.com>
2020-04-06 21:11:44 +0900

    챕터10. profile API 추가

--- src/main/java/com/noop/study/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/noop/study/springboot/config/auth/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // csrf().disable().headers().frameOptions().disable() -> h2-console 화면을 사용하기 위해 해당 옵션들을 disable 한다.
         http.csrf().disable().headers().frameOptions().disable().and().authorizeRequests()  // authorizeRequests() -> URL별 권한관리 설정옵션의 시작점
-                .antMatchers("/", "css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()   // permitAll() -> 해당 속성은 전체열람 권한
+                .antMatchers("/", "css/**", "/images/**", "/js/**", "/h2-console/**","/profile").permitAll()   // permitAll() -> 해당 속성은 전체열람 권한
                 .antMatchers("/api/**").hasRole(Role.USER.name())   // hasRole(Role.USER.name()) -> USER 권한이 있는 사람에게만 가능하게 한다.
                 .anyRequest().authenticated().and().logout().logoutSuccessUrl("/").and()    // 설정값 이외의 URL들을 나타낸다. authenticated() 추가시 로그인사용자만
                                                                                             // logout().logoutSuccessUrl("/") 로그아웃시 / 로 이동
